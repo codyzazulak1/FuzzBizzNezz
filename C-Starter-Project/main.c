56287f4bb9802ae8669949832e796de23c6ca5bb
codyzazulak1 <codyzazulak1@gmail.com>
2015-06-12 06:13:27 +0000

    Completed

--- C-Starter-Project/main.c
+++ C-Starter-Project/main.c
@@ -1,15 +1,38 @@
 //
 //  main.c
-//  C-Starter-Project
+//  FuzzBizzNezz
 //
-//  Created by Daniel Mathews on 2015-02-02.
-//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//  Created by Cody Zazulak.
+//  All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int x;
+    int nezz;
+    int fuzz;
+    int bizz;
+    
+    puts("Pick a number for Nezz:");
+    scanf("%d",&nezz);
+    puts("Pick a number for Fuzz:");
+    scanf("%d", &fuzz);
+    puts("Pick a number for Bizz:");
+    scanf ("%d", &bizz);
+    
+    for (x = 1; x <= nezz; x++) {
+        if ((x % fuzz == 0) && (x % bizz == 0)) {
+            printf("FuzzBizz\n");
+        } else if (x % fuzz == 0) {
+            printf("Fuzz\n");
+        } else if (x % bizz == 0)  {
+            printf("Bizz\n");
+        } else {
+            printf("%d\n", x);
+        }
+    }
+    
     return 0;
 }
